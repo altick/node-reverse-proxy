0d44c391965c374a414ac8d47238ba282de2c9a7
Petr Altman <altick87@hotmail.com>
2016-12-13 11:22:48 +0000

    first commit

---
+++ app/app.js
@@ -0,0 +1,35 @@
+var express  = require('express');
+var app      = express();
+var httpProxy = require('http-proxy');
+var cors = require('cors');
+//var http = require('http');
+var apiProxy = httpProxy.createProxyServer(
+  {
+    secure:false,
+    changeOrigin: true
+  }
+);
+var port = 3000;
+
+console.log('initializing...');
+
+var destServer = 'https://api.twitter.com'; ///'http://localhost:9000';
+
+app.use(cors());
+
+app.all("*", function(req, res) {
+    console.log(req.url + ' redirecting to ' + destServer);
+    apiProxy.web(req, res, {target: destServer});
+});
+
+console.log('Listening on: ' + port);
+app.listen(port);
+
+//
+// Create your target server
+//
+// http.createServer(function (req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.write('request successfully proxied!' + '\n' + req.originalUrl);
+//   res.end();
+// }).listen(9000);
